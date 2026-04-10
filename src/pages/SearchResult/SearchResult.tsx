09a8f6802fe3f52f6c1991168a500b851b1365fb
karuistep <karuistep@gmail.com>
2026-04-10 17:14:25 +0900

    feat: using mapper in SearchResult-component

--- src/pages/SearchResult/SearchResult.tsx
+++ src/pages/SearchResult/SearchResult.tsx
@@ -1,5 +1,6 @@
 import { useSearchParams } from 'react-router-dom';
 import { useWeather } from '../../hooks/useWeather';
+import { mapCondition, mapHumidity, mapWind } from '../../domain/weather.rules';
 import { SearchBar } from '../../components/SearchBar/SearchBar';
 import { WeatherCard } from '../../components/weather/WeatherCard/WeatherCard';
 import NoData from '../../components/NoData';
@@ -9,17 +10,23 @@ import styles from './SearchResult.module.scss';
 const SearchResult = () => {
   const [searchParams] = useSearchParams();
   const city = searchParams.get('city');
-  const { data, isLoading, isError } = useWeather(city || '');
+  const { data: weather, isLoading, isError } = useWeather(city || '');
+  const uiWeather = weather && {
+    ...weather,
+    conditionKey: mapCondition(weather.conditionId),
+    humidityKey: mapHumidity(weather.humidity),
+    windKey: mapWind(weather.windSpeed ?? 0),
+  };
 
   if (isLoading) return <div>Loading...</div>;
-  if (!city || isError || !data) return <NoData />;
+  if (!city || isError || !uiWeather) return <NoData />;
 
   return (
     <div className={styles.container}>
       <SearchBar />
       <div className={styles['weather-contents-row']}>
         <div className={styles['weather-card-container']}>
-          <WeatherCard weather={data} />
+          <WeatherCard weather={uiWeather} />
         </div>
       </div>
     </div>
